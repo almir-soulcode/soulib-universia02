5e9b67951b9fe1fcf0fb270458a72a33ea91007d
jose-almir <jr.cod.dev@gmail.com>
2024-01-23 14:00:32 -0300

    aula_08: inserção de empréstimo (post)

--- src/main/java/com/soulcode/soulib/controllers/EmprestimoController.java
+++ src/main/java/com/soulcode/soulib/controllers/EmprestimoController.java
@@ -1,10 +1,13 @@
 package com.soulcode.soulib.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.soulcode.soulib.models.Cliente;
@@ -39,5 +42,28 @@ public class EmprestimoController {
         return mv;
     }
 
-    // Ação de inserir empréstimo
+    @PostMapping("/emprestimos/create")
+    public String createEmprestimo(@RequestParam Integer idCliente, @RequestParam Integer idLivro, Emprestimo emprestimo) {
+        try {
+            Optional<Cliente> clienteOpt = clienteRepository.findById(idCliente);
+            Optional<Livro> livroOpt = livroRepository.findById(idLivro);
+
+            // Para inserir o empréstimo, cliente e livro devem existir
+            if(clienteOpt.isPresent() && livroOpt.isPresent()) {
+                Cliente cliente = clienteOpt.get();
+                Livro livro = livroOpt.get();
+                
+                // Associamos o cliente ao empréstimo
+                emprestimo.setCliente(cliente);
+
+                // Associamos o livro ao empréstimo
+                emprestimo.setLivroEmprestado(livro);
+
+                emprestimoRepository.save(emprestimo);
+            }
+        } catch(Exception ex) {
+            return "erro";
+        }
+        return "redirect:/emprestimos";
+    }
 }
